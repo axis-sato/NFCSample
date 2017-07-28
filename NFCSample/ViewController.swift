c7f6cb2237477e3f75cd00317f2e3006d839e3d2
c8112002 <c8112002@gmail.com>
2017-07-28 23:46:23 +0900

    Implement NFC tag reading

--- NFCSample/ViewController.swift
+++ NFCSample/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import CoreNFC
 
 class ViewController: UIViewController {
+    var session: NFCNDEFReaderSession!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        session = NFCNDEFReaderSession(delegate: self,
+                                       queue: nil,
+                                       invalidateAfterFirstRead: true)
+        session.begin()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+
+extension ViewController: NFCNDEFReaderSessionDelegate {
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for record in message.records {
+                if let type = String.init(data: record.type, encoding: .utf8) {
+                    print("type: \(type)")
+                }
+                if let identifier = String.init(data: record.identifier, encoding: .utf8) {
+                    print("identifier: \(identifier)")
+                }
+                if let payload = String.init(data: record.payload, encoding: .utf8) {
+                    print("payload: \(payload)")
+                }
+            }
+        }
+    }
+    
+    public func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print(#function, error)
+    }
+}
